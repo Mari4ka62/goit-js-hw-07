57ddd5582d11a14a2abc7c995a42f20c06a221e1
Maria Vovchuk <mari4ka62@gmail.com>
2023-03-03 20:44:27 +0200

    task1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+
+const items = galleryItems.map(item => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+}).join('');
+gallery.innerHTML = items;
+
+gallery.addEventListener('click', onImgClick);
+
+function onImgClick(event) {
+    event.preventDefault();
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" 
+    width="1280" 
+    height="850">
+`)
+
+instance.show()
+}
